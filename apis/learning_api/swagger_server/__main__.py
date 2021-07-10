30e24a0cac598c95e954533cb179f2259bee6e58
Miguel Dias <miguel.dias.1212@gmail.com>
2021-07-10 14:17:57 +0100

    My first commit

--- apis/learning_api/swagger_server/__main__.py
+++ apis/learning_api/swagger_server/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import connexion
-
+import os
 from swagger_server import encoder
 
 
@@ -9,7 +9,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'FakeUsers API'}, pythonic_params=True)
-    app.run(port=8080)
+    app.run(port=os.environ['PORT'])
 
 
 if __name__ == '__main__':
